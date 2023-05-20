6701af5f17499cb008127807e7b6d02c93db847c
Dingo <Test@testing.com>
2023-05-20 03:38:03 -0600

    Walkthrough Test

--- main.py
+++ main.py
@@ -263,6 +263,6 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-
+# host='0.0.0.0', port=5000
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run()
